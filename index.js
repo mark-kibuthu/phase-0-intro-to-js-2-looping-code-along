65e160864e749a4e048adc5b2594b3bbbe030c15
Agent-grey <markkibuthu@gmail.com>
2024-07-22 14:18:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    names.forEach(name => {
+      let message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    });
+    return thankYouMessages;
+  }
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
